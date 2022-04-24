f9618701a1012b38b4d6a172bdcb95375fa626b0
Alejandro-FA <61871580+Alejandro-FA@users.noreply.github.com>
2022-04-24 11:01:27 +0000

    Remove compilation files from repository

--- lab1_T1G3/1_dot_product/dotp_job.cmd
+++ lab1_T1G3/1_dot_product/dotp_job.cmd
@@ -3,7 +3,7 @@
 #SBATCH --output=out_dotp.out
 #SBATCH --error=out_dotp.err
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=8
 #SBATCH --time=00:00:10
 
 source /etc/profile.d/z00-global-profile.sh
@@ -12,5 +12,5 @@ module load GCC/10.2.0
 
 make clean >> out_dotp.err && make >> out_dotp.err || exit 1 
 
-./a.out 1 1 100000000 >> results.out
+./a.out 2 8 100000000 >> results.out
 
