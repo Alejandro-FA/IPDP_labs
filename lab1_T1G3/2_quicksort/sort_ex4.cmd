e4906731ab7039bd9494b18b58bf2424bfff5968
Alejandro-FA <61871580+Alejandro-FA@users.noreply.github.com>
2022-05-01 11:54:43 +0000

    ex5 computations are now obtained with the new version of quicksort.c

--- lab1_T1G3/2_quicksort/sort_ex4.cmd
+++ lab1_T1G3/2_quicksort/sort_ex4.cmd
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=test
-#SBATCH --output=out_sort.out
+#SBATCH --output=out_sort_ex4.txt
 #SBATCH --error=out_sort.err
-#SBATCH --ntasks=2
+#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --time=00:10:00
 
@@ -14,7 +14,6 @@ make clean >> out_sort.err && make >> out_sort.err || exit 1
 
 nruns=5
 
-echo "Exercise 4"
 for size in 1000000 100000000
 do
     for nthreads in 1 2 4 8 16
@@ -25,22 +24,5 @@ do
             ./a.out 1 $nthreads $size 0
         done
         echo "--------------------"
-    done
-done
-
-echo -e "\n\nExercise5"
-for size in 1000000 100000000
-do
-    for X in 5 100 1000
-    do
-        for nthreads in 1 2 4 8 16
-        do
-            echo "Size: ${size}, Threads: ${nthreads}, X: ${X}"
-            for (( i = 0; i < $nruns; i++ ))
-            do
-                ./a.out 2 $nthreads $size $X
-            done
-            echo "--------------------"
-        done
     done
 done
---
+++ lab1_T1G3/2_quicksort/sort_ex5.cmd
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=test
+#SBATCH --output=out_sort_ex5.txt
+#SBATCH --error=out_sort.err
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --time=00:20:00
+
+source /etc/profile.d/z00-global-profile.sh
+
+module load GCC/10.2.0
+
+make clean >> out_sort.err && make >> out_sort.err || exit 1 
+
+nruns=1
+
+for size in 1000000 100000000
+do
+    for X in 5 100 1000
+    do
+        for nthreads in 1 2 4 8 16
+        do
+            echo "Size: ${size}, Threads: ${nthreads}, X: ${X}"
+            for (( i = 0; i < $nruns; i++ ))
+            do
+                ./a.out 2 $nthreads $size $X
+            done
+            echo "--------------------"
+        done
+    done
+done
